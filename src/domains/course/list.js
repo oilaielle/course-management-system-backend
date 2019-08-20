18f7676a38c68ef8d73806256444c61171fa7f53
oilaielle <suwit.ruknukul@gmail.com>
2019-08-21 04:28:37 +0700

    update

--- src/domains/course/list.js
+++ src/domains/course/list.js
@@ -2,9 +2,6 @@ import courseRepository from '../../models/course.repository'
 import type { Paginate } from '../../types/paginate'
 
 export default async (filter: any, options: Paginate) => {
-  console.log('filter--------\n', filter)
-  console.log('options--------\n', options)
-
   const resp = await courseRepository.find(filter)
   return resp
 }
