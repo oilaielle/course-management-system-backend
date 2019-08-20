b8dea09fbe855e0761226dcb51e4e0174bc814c0
oilaielle <suwit.ruknukul@gmail.com>
2019-08-20 16:22:43 +0700

    update

--- src/domains/user/update.js
+++ src/domains/user/update.js
@@ -2,9 +2,11 @@ import userRespository from '../../models/user.repository'
 import { checkUpdate, findOneIfNotExistThrowError } from '../../utils/domain'
 
 const updateUserProfile = async (id, body) => {
-  await findOneIfNotExistThrowError(userRespository, {_id:id})
+  console.log('body--------------\n', body)
+
+  await findOneIfNotExistThrowError(userRespository, { _id: id })
   const data = await checkUpdate(userRespository, { _id: id }, body)
-  
+
   return data
 }
 
