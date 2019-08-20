19067fa4704ce1a699bad5acb50b6f12e13a4c3d
oilaielle <suwit.ruknukul@gmail.com>
2019-08-20 23:50:34 +0700

    update

--- src/controllers/system/system.controller.js
+++ src/controllers/system/system.controller.js
@@ -1,15 +1,15 @@
-// import { HttpMethod, route } from '@spksoft/koa-decorator'
-// import mongoose from 'mongoose'
+import { HttpMethod, route } from '@spksoft/koa-decorator'
+import mongoose from 'mongoose'
 
-// @route('/v1/system')
-// export default class SystemController {
-//   @route('/health', HttpMethod.GET)
-//   async health(ctx) {
-//     if (mongoose.connection.readyState === 0) {
-//       throw Error('Error')
-//     }
-//     ctx.body = {
-//       databaseStatus: mongoose.connection.readyState,
-//     }
-//   }
-// }
+@route('/v1/system')
+export default class SystemController {
+  @route('/health', HttpMethod.GET)
+  async health(ctx) {
+    if (mongoose.connection.readyState === 0) {
+      throw Error('Error')
+    }
+    ctx.body = {
+      databaseStatus: mongoose.connection.readyState,
+    }
+  }
+}
